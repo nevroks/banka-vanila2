29a0248d5921a3933a200daf5a782e5415cc8e6b
Nikita Babitsyn <nitubro1617@gmail.com>
2024-12-15 21:32:41 +0500

    раб ота над банкой

--- js/sections/banka.js
+++ js/sections/banka.js
@@ -20,12 +20,13 @@ $(function() {
             const step4Duration = 600;
             const step5Duration = 1100;
             const step6Duration = 900;
-            const step7Duration = 900;
+            const step7Duration = 600;
             const step8Duration = 900;
+            const step9Duration = 900;
 
             // ----- 1 шаг -----
             bankaWrapper.classList.add('step-1');
-
+            bankaWrapper.classList.remove('glow');
             setTimeout(() => {
                 // ----- 2 шаг -----
                 bankaLid.classList.add('step-2');
@@ -59,12 +60,16 @@ $(function() {
             setTimeout(() => {
                 // ----- 8 шаг -----
                 cookie.classList.add('step-8');
-                form.classList.add('step-8');
             }, step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration)
             setTimeout(() => {
                 // ----- 9 шаг -----
+                cookie.classList.add('step-9');
                 form.classList.add('step-9');
             }, step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration + step8Duration)
+            setTimeout(() => {
+                // ----- 10 шаг -----
+                form.classList.add('step-10');
+            }, step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration + step8Duration + step9Duration)
         }
 
 
@@ -102,6 +107,7 @@ $(function() {
     // 4. Печенье подлетает в верх | длительность: 0.6 с
     // 5. Банка уходит в opacity 0, печенье центруется | длительность: 1.1 с
     // 6. Печенье увеличивается | длительность: 0.9 с
-    // 7. Печенье раскалывается | длительность: 0.9 с
-    // 8. Дольки печенья разьезжаются по сторонам, убирается display:none у формы | длительность: 0.9 с
-    // 9. Появляется белая форма
+    // 7. Печенье потярясывается | длительность: 0.6 с
+    // 8. Печенье раскалывается | длительность: 0.9 с
+    // 9. Дольки печенья разьезжаются по сторонам, убирается display:none у формы | длительность: 0.9 с
+    // 10. Появляется белая форма
