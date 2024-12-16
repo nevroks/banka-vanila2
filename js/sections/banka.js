20c5721fd11bba7052e4a8a54d06e2186bff5f34
Nikita Babitsyn <nitubro1617@gmail.com>
2024-12-16 21:28:12 +0500

    Работа с банкой

--- js/sections/banka.js
+++ js/sections/banka.js
@@ -1,4 +1,10 @@
 $(function() {
+
+        const main = document.querySelector("main")
+        const bankaSectionTitle = document.querySelector(".banka-banka-title")
+
+
+
         const bankaWrapper = document.querySelector('.banka-banka-wrapper');
         const bankaLid = document.querySelector('.banka-banka-lid');
         const cookie = document.querySelector('.banka-banka-specialCookie');
@@ -7,13 +13,12 @@ $(function() {
         bankaWrapper.addEventListener('click', startAnimation);
 
         function startAnimation() {
-            console.log("start");
-
             bankaWrapper.removeEventListener('click', startAnimation);
-            // ---убрать скролл надо---
+            bankaWrapper.classList.remove('glow');
+            main.classList.add("scroll-block")
 
-            // ---убрать скролл надо---
 
+            const step0Duration = 1100;
             const step1Duration = 1100;
             const step2Duration = 900;
             const step3Duration = 900;
@@ -24,52 +29,55 @@ $(function() {
             const step8Duration = 900;
             const step9Duration = 900;
 
-            // ----- 1 шаг -----
-            bankaWrapper.classList.add('step-1');
-            bankaWrapper.classList.remove('glow');
+            bankaSectionTitle.classList.add("step-0")
+                // ----- 1 шаг -----
+            setTimeout(() => {
+                bankaWrapper.classList.add('step-1');
+            }, step0Duration)
+
             setTimeout(() => {
                 // ----- 2 шаг -----
                 bankaLid.classList.add('step-2');
 
-            }, step1Duration)
+            }, step0Duration + step1Duration)
             setTimeout(() => {
                 // ----- 3 шаг -----
                 bankaLid.classList.add('step-3');
                 cookie.classList.add('step-3');
-            }, step1Duration + step2Duration)
+            }, step0Duration + step1Duration + step2Duration)
             setTimeout(() => {
                 // ----- 4 шаг -----
                 cookie.classList.add('step-4');
                 setTimeout(() => {
                     cookie.classList.add('z-index');
                 }, step4Duration / 1.7)
-            }, step1Duration + step2Duration + step3Duration)
+            }, step0Duration + step1Duration + step2Duration + step3Duration)
             setTimeout(() => {
                 // ----- 5 шаг -----
                 bankaWrapper.classList.add('step-5');
                 cookie.classList.add('step-5');
-            }, step1Duration + step2Duration + step3Duration + step4Duration)
+            }, step0Duration + step1Duration + step2Duration + step3Duration + step4Duration)
             setTimeout(() => {
                 // ----- 6 шаг -----
                 cookie.classList.add('step-6');
-            }, step1Duration + step2Duration + step3Duration + step4Duration + step5Duration)
+            }, step0Duration + step1Duration + step2Duration + step3Duration + step4Duration + step5Duration)
             setTimeout(() => {
                 // ----- 7 шаг -----
                 cookie.classList.add('step-7');
-            }, step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration)
+            }, step0Duration + step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration)
             setTimeout(() => {
                 // ----- 8 шаг -----
                 cookie.classList.add('step-8');
-            }, step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration)
+            }, step0Duration + step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration)
             setTimeout(() => {
                 // ----- 9 шаг -----
                 cookie.classList.add('step-9');
                 form.classList.add('step-9');
-            }, step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration + step8Duration)
+            }, step0Duration + step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration + step8Duration)
             setTimeout(() => {
                 // ----- 10 шаг -----
                 form.classList.add('step-10');
-            }, step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration + step8Duration + step9Duration)
+            }, step0Duration + step1Duration + step2Duration + step3Duration + step4Duration + step5Duration + step6Duration + step7Duration + step8Duration + step9Duration)
         }
 
 
@@ -101,7 +109,8 @@ $(function() {
         })
     })
     // Короче, шаги анимации:
-    // 1. Приближается камера банка немного опускается вниз, перестраивается хедер | длительность: 1.1 с
+    // 0. Пропадает discover your fortune | длительность: 1.1 с
+    // 1. Приближается камера банка немного опускается вниз | длительность: 1.1 с
     // 2. Крышка как бы приподнимается и отлетает чуть влево | длительность: 0.9 с
     // 3. Крышка отлевает влево и поворачивается, появляется печенье| длительность: 0.9 с
     // 4. Печенье подлетает в верх | длительность: 0.6 с
