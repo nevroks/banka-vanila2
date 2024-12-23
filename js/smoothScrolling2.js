bd56929ac7f5b3e29373e150562993e3220a8499
Reva63 <ian.svintsov@yandex.ru>
2024-12-23 05:54:58 +0400

    фиксы говна

--- js/smoothScrolling2.js
+++ js/smoothScrolling2.js
@@ -4,51 +4,79 @@ let stepsForThirdBlock = 0;
 let secondBlock = document.querySelector('.second');
 let thirdBlock = document.querySelector('.third');
 
+let isTouchDevice = false;
+let isMouseDevice = true;
+
+function detectTrackPad(e) {
+    if (e.wheelDeltaY) {
+        if (e.wheelDeltaY === (e.deltaY * -3)) {
+            isTouchDevice = true;
+            isMouseDevice = false;
+        }
+    }
+    else if (e.deltaMode === 0) {
+        isTouchDevice = true;
+        isMouseDevice = false;
+    } else {
+        isMouseDevice = true;
+    }
+    console.log(isTouchDevice ? "Trackpad detected" : "Mousewheel detected");
+}
+
+document.addEventListener("mousewheel", detectTrackPad, false);
+document.addEventListener("DOMMouseScroll", detectTrackPad, false);
+
 if (window.innerWidth > 1024) {
-    window.addEventListener('wheel', (e) => {
+    setInterval(() => {
         let activeSections = document.querySelectorAll('.activeSection');
+        if (activeSections[activeSections.length - 1].getAttribute('sectionId') == "2" && isMouseDevice) {
+            window.addEventListener('wheel', (e) => {
+                let step = 40;
 
-        if (activeSections[activeSections.length - 1].getAttribute('sectionId') == "2") {
-            let step = 40;
-
-            if (stepsForThirdBlock < 6) {
-                setTimeout(() => blockedScrollDown = true, 750);
-            } else if (stepsForThirdBlock == 6) {
-                setTimeout(() => blockedScrollDown = false, 750);
-            }
-
-            if (stepsForSecondBlock > 0) {
-                setTimeout(() => blockedScrollUp = true, 750);
-            } else if (stepsForSecondBlock == 0) {
-                setTimeout(() => blockedScrollUp = false, 750);
-            }
-
-            if (e.deltaY > 0) {
-                if (stepsForSecondBlock <= 5) {
-                    stepsForSecondBlock += 1;
-                    secondBlock.style.transform = `translateY(-${stepsForSecondBlock * step}px)`;
-                    thirdBlock.style.transform = `translateY(-${stepsForSecondBlock * step}px)`;
-                } else {
-                    if (stepsForThirdBlock <= 5) {
-                        stepsForThirdBlock += 1;
-                        thirdBlock.style.transform = `translateY(-${(stepsForSecondBlock * step) + (stepsForThirdBlock * step)}px)`;
-                    }
+                if (stepsForThirdBlock < 5) {
+                    setTimeout(() => blockedScrollDown = true, 750);
+                } else if (stepsForThirdBlock >= 5) {
+                    setTimeout(() => blockedScrollDown = false, 750);
                 }
-            } else if (e.deltaY <= 0) {
-                if (stepsForThirdBlock > 0) {
-                    stepsForThirdBlock -= 1;
-                    thirdBlock.style.transform = `translateY(-${(stepsForSecondBlock * step) + (stepsForThirdBlock * step)}px)`;
-                } else {
-                    if (stepsForSecondBlock > 0) {
-                        stepsForSecondBlock -= 1;
-                        thirdBlock.style.transform = `translateY(-${stepsForSecondBlock * step}px)`;
+
+                if (stepsForSecondBlock > 0) {
+                    setTimeout(() => blockedScrollUp = true, 750);
+                } else if (stepsForSecondBlock == 0) {
+                    setTimeout(() => blockedScrollUp = false, 750);
+                }
+
+                console.log("Touch: " + isTouchDevice);
+                console.log("Mouse: " + isMouseDevice);
+
+                if (e.deltaY > 0) {
+                    console.log('mouse down')
+                    if (stepsForSecondBlock <= 5) {
+                        stepsForSecondBlock += 1;
                         secondBlock.style.transform = `translateY(-${stepsForSecondBlock * step}px)`;
+                        thirdBlock.style.transform = `translateY(-${stepsForSecondBlock * step}px)`;
+                    } else {
+                        if (stepsForThirdBlock <= 5) {
+                            stepsForThirdBlock += 1;
+                            thirdBlock.style.transform = `translateY(-${(stepsForSecondBlock * step) + (stepsForThirdBlock * step)}px)`;
+                        }
+                    }
+                } else if (e.deltaY <= 0) {
+                    console.log('mouse up')
+                    if (stepsForThirdBlock > 0) {
+                        stepsForThirdBlock -= 1;
+                        thirdBlock.style.transform = `translateY(-${(stepsForSecondBlock * step) + (stepsForThirdBlock * step)}px)`;
+                    } else {
+                        if (stepsForSecondBlock > 0) {
+                            stepsForSecondBlock -= 1;
+                            thirdBlock.style.transform = `translateY(-${stepsForSecondBlock * step}px)`;
+                            secondBlock.style.transform = `translateY(-${stepsForSecondBlock * step}px)`;
+                        }
                     }
                 }
-            }
+            })
         } else {
             blockedScrollUp = false;
             blockedScrollDown = false;
         }
-    })
+    }, 200);
 }
